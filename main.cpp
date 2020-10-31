a6c72d59a4111b87b959ebe37ceaa10c18241724
patkub2 <patrykkubala1999@gmail.com>
2020-10-31 11:29:27 +0100

    arrays pointers

--- main.cpp
+++ main.cpp
@@ -4,12 +4,30 @@
 
 using namespace std;
 
+void print_contents(vector<int> &input)
+{
+    for (int i = 0; i < input.size(); i++)
+    {
+        cout << input[i] << endl;
+    }
+}
+
 int main()
 {
-    int variable = 8;
-    int *ptr;
-    ptr = &variable;
-    *ptr = 25;
-    cout << variable;
+
+    int size;
+
+    cout << "Provide a size for your array: " << endl;
+    cin >> size;
+
+    vector<int> numbers;
+
+    for (int i = 0; i < size; i++)
+    {
+        numbers.push_back(i);
+    }
+
+    print_contents(numbers);
+
     return 0;
 }
